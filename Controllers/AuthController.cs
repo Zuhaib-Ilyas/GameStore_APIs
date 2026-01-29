29425524f6e881182b1ce2f86405a50722d5ca52
Zuhaib-Ilyas <zuhaibilyas5upwork@gmail.com>
2026-01-30 00:24:16 +0500

    Added generateToken func

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,12 +3,16 @@ using GameStore_API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace GameStore_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
         public static User user = new();
 
@@ -41,8 +45,74 @@ namespace GameStore_API.Controllers
 
             }
 
-            string token = "success";
+            string token = CreateToken(user);
             return Ok(token);
+        }
+
+        //private string CreateToken(User user)
+        //{
+        //    var claims = new List<Claim>
+        //    {
+        //            new Claim(ClaimTypes.Name, user.UserName)
+        //    };
+
+        //    var key = new SymmetricSecurityKey(
+        //        Encoding.UTF8.
+        //        GetBytes(configuration.GetValue<string>("AppSettings:Token")!)
+        //        );
+
+        //    var securityAlgo = SecurityAlgorithms.HmacSha512;
+
+        //    var creds = new SigningCredentials(
+        //        key, securityAlgo);
+
+
+
+        //    var tokenDescriptor = new JwtSecurityToken(
+        //        issuer: configuration.GetValue<string>("AppSettings:issuer"),
+        //        audience: configuration.GetValue<string>("AppSettings:issuer"),
+        //        claims: claims,
+        //        expires: DateTime.UtcNow.AddDays(1),
+        //        signingCredentials: creds
+
+        //        );
+
+        //    var token = new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
+        //    return token;   
+        //}
+
+        private string CreateToken(User user)
+        {
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName)
+            };
+
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes
+                (configuration.GetValue<string>("AppSettings:Token")!)
+                
+                );
+
+            var algorithm = SecurityAlgorithms.HmacSha512;
+            var creds = new SigningCredentials(key, algorithm);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:issuer"),
+                audience: configuration.GetValue<string>("AppSettings:audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                
+                );
+
+
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
+            return token;
         }
     }
 }
